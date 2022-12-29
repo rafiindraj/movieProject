b1d5b582de79d1fcdcd6e1d7f844cb072092bd9e
Rafiindraj <v-rafi.indrajati@anteraja.id>
2022-12-29 07:16:41 +0700

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,21 @@
+// import React from 'react'
+
+function Header() {
+  return (
+    <header>
+      <div>
+      <img
+          src="https://rb.gy/ulxxee"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+      </div> 
+      <div>
+
+      </div>
+    </header>
+  );
+}
+
+export default Header;
